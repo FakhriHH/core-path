161e0aea146363fcffc77328c72dea4338814c42
aghnmaulina <aghnia844@gmail.com>
2025-01-08 13:37:09 +0700

    schedule

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/schedule', scheduleRoutes);
+app.use('/api/schedule', scheduleRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
