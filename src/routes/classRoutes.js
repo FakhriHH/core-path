a0f2fc0c4ccbf28356134f410cbe83993fd33bfc
FakhriHH <fakhrihabib9@gmail.com>
2025-01-14 18:27:51 +0700

    Membuat Routes Class

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -1,37 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const {
-  getAllClasses,
-  getClassById,
-  createClass,
-  updateClass,
-  deleteClass,
-} = require('../controllers/classController');
 const { authenticateToken, authorizeRole } = require('../middlewares/authMiddleware');
 
-// Endpoint dengan akses role
-router.get('/', authenticateToken, getAllClasses); // Semua user bisa mengakses
-router.get('/:id', authenticateToken, getClassById); // Semua user bisa mengakses
+const express = require('express');
+const router = express.Router();
+const { createClass, getClasses, updateClass, deleteClass, getClassesByCategory, getClassesByRole, getClassesByLevel } = require('../controllers/classController');
 
-router.post(
-  '/',
-  authenticateToken,
-  authorizeRole([1, 2]), // Hanya admin (1) dan guru (2)
-  createClass
-);
-
-router.put(
-  '/:id',
-  authenticateToken,
-  authorizeRole([1, 2]), // Hanya admin (1) dan guru (2)
-  updateClass
-);
-
-router.delete(
-  '/:id',
-  authenticateToken,
-  authorizeRole([1]), // Hanya admin (1)
-  deleteClass
-);
+// Route untuk CRUD kelas
+router.post('/create', authenticateToken, createClass);  // Hanya admin
+router.get('/list', authenticateToken, getClasses);       // Semua bisa lihat
+router.put('/update/:id_class', authenticateToken, updateClass);  // Hanya admin
+router.delete('/delete/:id_class', authenticateToken, deleteClass); // Hanya admin
+router.get('/category/:categoryId', authenticateToken, getClassesByCategory);
+router.get('/role/:roleId', authenticateToken, getClassesByRole);
+router.get('/level/:levelId', authenticateToken, getClassesByLevel);
 
 module.exports = router;
