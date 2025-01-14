6f17787cbc0d41cfaecf04db61e5133f47f67e99
zuzuaulia <zulfaaul@gmail.com>
2025-01-14 21:21:32 +0700

    LoginPage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
 const app = express();
@@ -8,6 +9,16 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'pages', 'LoginPage.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets', 'SignUp.html'));
+});
 
 app.use('/api/auth', authRoutes);
 
