07d665d55717481ad95d1e8cac36ac6d60bdfc86
Faris Arrasid <A2.2000040@mhs.stmik-sumedang.ac.id>
2025-01-16 22:29:55 +0700

    add script in detailClass.html

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
 const classRoutes = require('./src/routes/classRoutes');
+const path = require('path');
 const app = express();
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/core-path', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'section', 'detailClass.html'));
+});
+
+app.get('/aboutUs.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'section', 'aboutUs.html'));
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/classes', classRoutes); 
