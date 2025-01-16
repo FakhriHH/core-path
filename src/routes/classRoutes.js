521cfe93957622d18e0899f3f5954d40431e1093
FakhriHH <fakhrihabib9@gmail.com>
2025-01-16 18:15:26 +0700

    menambahkan role di rute

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const { createClass, getClasses, updateClass, deleteClass, getClassesByCategory, getClassesByRole, getClassesByLevel } = require('../controllers/classController');
 
 // Route untuk CRUD kelas
-router.post('/create', authenticateToken, createClass);  // Hanya admin
+router.post('/create', authenticateToken,authorizeRole([1]), createClass);  // Hanya admin
 router.get('/list', authenticateToken, getClasses);       // Semua bisa lihat
-router.put('/update/:id_class', authenticateToken, updateClass);  // Hanya admin
-router.delete('/delete/:id_class', authenticateToken, deleteClass); // Hanya admin
+router.put('/update/:id_class', authenticateToken, authorizeRole([1]), updateClass);  // Hanya admin
+router.delete('/delete/:id_class', authenticateToken, authorizeRole([1]),  deleteClass); // Hanya admin
 router.get('/category/:categoryId', authenticateToken, getClassesByCategory);
 router.get('/role/:roleId', authenticateToken, getClassesByRole);
 router.get('/level/:levelId', authenticateToken, getClassesByLevel);
