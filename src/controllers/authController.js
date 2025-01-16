5eac1d3e23b0b8d5626d8f2f29c94eed39b4c707
FakhriHH <fakhrihabib9@gmail.com>
2025-01-16 18:12:37 +0700

    tambahan untuk message login dan register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -62,12 +62,12 @@ const login = async (req, res) => {
     const user = await User.getUserByEmail(email);
 
     if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: 'Email not Found' });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: 'Incorrect Password' });
     }
 
     const token = jwt.sign({ id: user.id_user, role: user.id_role }, process.env.JWT_SECRET, { expiresIn: '1h' });
