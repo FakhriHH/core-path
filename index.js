0582db6ecc889cba3758f94d1ef90423948d7f3d
aghnmaulina <aghnia844@gmail.com>
2025-01-17 07:42:42 +0700

    update endpoint

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
 const scheduleRoutes = require('./src/routes/scheduleRoutes');
+const bookingRoutes = require('./src/routes/bookingRoutes');
+const classRoutes = require('./src/routes/classRoutes');
 const app = express();
 
 dotenv.config();
@@ -12,6 +14,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/schedule', scheduleRoutes);
+app.use('/api/booking', bookingRoutes);
+app.use('/api/classes', classRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
